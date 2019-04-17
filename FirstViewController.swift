0f86ce599e75e142a74a0177e726674a316b109b
Jim Shi <shiyilin2008@gmail.com>
2019-04-17 23:52:06 +0800

    Initial commit.

---
+++ FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MJWorkChecker3
+//
+//  Created by Jim Macintosh Shi on 4/17/19.
+//  Copyright © 2019 Creative Sub. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MJWorkChecker3
+//
+//  Created by Jim Macintosh Shi on 4/17/19.
+//  Copyright © 2019 Creative Sub. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
