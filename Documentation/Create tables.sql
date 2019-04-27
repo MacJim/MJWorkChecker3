bc10475699f45ad3346bfe35e0e49f8fd071399c
Jim Shi <shiyilin2008@gmail.com>
2019-04-27 20:01:25 +0800

    The database structure is going to get reworked in the following commits.
    In this commit:
    
    Updated create tables statements: Added a new `Days` table that stores each day's total working duration. This should eliminate most read access to the old, cumbersome `WorkSegments` table and improve performance significantly.

--- Documentation/Create tables.sql
+++ Documentation/Create tables.sql
@@ -1,3 +1,24 @@
+/**
+ * Total working duration of each day.
+ */
+CREATE TABLE IF NOT EXISTS Days (
+    dayID INTEGER,
+
+    year INTEGER,    -- 2019
+
+    month INTEGER,    -- 11
+
+    day INTEGER,    -- 29
+
+    totalWorkingDuration INTEGER NOT NULL,    -- Total working duration on that specific day.
+
+
+    PRIMARY KEY (dayID)
+);
+
+-- CREATE INDEX IF NOT EXISTS Days_month ON Days(month);    -- Currently not used.
+
+
 CREATE TABLE IF NOT EXISTS WorkSegments (
     segmentID INTEGER,
 
@@ -5,11 +26,16 @@ CREATE TABLE IF NOT EXISTS WorkSegments (
 
     stopWorkingTimestamp INTEGER NOT NULL,    -- Stop working UNIX timestamp.
 
+    dayID INTEGER,
 
-    PRIMARY KEY (segmentID)
+
+    PRIMARY KEY (segmentID),
+
+    FOREIGN KEY (dayID) REFERENCES Days(dayID) ON DELETE SET NULL
 );
 
-
-CREATE INDEX IF NOT EXISTS WorkSegments_startWorkingTimestamp ON WorkSegments (startWorkingTimestamp);
+-- CREATE INDEX IF NOT EXISTS WorkSegments_startWorkingTimestamp ON WorkSegments (startWorkingTimestamp);    -- No longer used.
 
 -- CREATE INDEX IF NOT EXISTS WorkSegments_stopWorkingTimestamp ON WorkSegments (stopWorkingTimestamp);    -- Currently not used.
+
+CREATE INDEX IF NOT EXISTS WorkSegments_dayID ON WorkSegments(dayID);
